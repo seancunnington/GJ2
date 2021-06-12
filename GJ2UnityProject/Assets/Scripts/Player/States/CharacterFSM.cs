01eef2d46a05219cc726ebfee736c0cec9310e24
socknot <38506114+seancunnington@users.noreply.github.com>
2021-06-11 18:29:40 -0700

    Dual Character Controller Complete
    Able to control 2 characters at once, each with their own camera.

---
+++ GJ2UnityProject/Assets/Scripts/Player/States/CharacterFSM.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterPhysics))]
+public class CharacterFSM : MonoBehaviour
+{
+     #region Public Variables
+
+     public string currentStateName = "";
+
+     //states
+
+     //input
+     [HideInInspector] public CharacterPhysics _characterPhysics;
+
+     #endregion
+
+     #region Private Variables
+
+     //current state
+
+     #endregion
+
+
+
+     private void Awake()
+     {
+          _characterPhysics = GetComponent<CharacterPhysics>();
+     }
+
+
+     private void OnEnable()
+     {
+          
+     }
+
+
+     private void Update()
+     {
+          
+     }
+}
